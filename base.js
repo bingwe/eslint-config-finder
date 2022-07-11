7162c3394010fc109b14d73d15a7950a00aa842d
bingwe <bingwe.hu@icloud.com>
2022-07-12 02:29:55 +0800

    :art: 改进别名配置生成结构

--- base.js
+++ base.js
@@ -1,3 +1,67 @@
+const fs = require('fs')
+const path = require('path')
+const resolve = require('resolve')
+
+const appDirectory = fs.realpathSync(process.cwd())
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath)
+
+const filePaths = {
+  appPath: resolveApp('.'),
+  appTsConfig: resolveApp('tsconfig.json'),
+  appJsConfig: resolveApp('jsconfig.json'),
+  appNodeModules: resolveApp('node_modules'),
+}
+
+// 去掉通配符
+const takeOutMatching = (str) => (str || '').replace(/\/\*.*/, '')
+
+function getAliasMap() {
+  // 是否存在js/ts配置文件
+  const hasTsConfig = fs.existsSync(filePaths.appTsConfig)
+  const hasJsConfig = fs.existsSync(filePaths.appJsConfig)
+
+  if (hasTsConfig && hasJsConfig) {
+    throw new Error('tsconfig.json 和 jsconfig.json同时存在，请删除其一')
+  }
+
+  let config
+
+  // tsconfig优先级比较高
+  if (hasTsConfig) {
+    const ts = require(resolve.sync('typescript', {
+      basedir: filePaths.appNodeModules,
+    }))
+    config = ts.readConfigFile(filePaths.appTsConfig, ts.sys.readFile).config
+  } else if (hasJsConfig) {
+    // jsconfig
+    config = require(filePaths.appJsConfig)
+  }
+
+  // 未找到配置文件
+  if (!config) {
+    return []
+  }
+
+  config = config || {}
+  const options = config.compilerOptions || {}
+  const { baseUrl, paths } = options
+  if (!baseUrl || !paths) {
+    return []
+  }
+
+  const result = []
+  Object.entries(paths).forEach(([key, value]) => {
+    if (Array.isArray(value) && value.length > 0) {
+      result.push([
+        takeOutMatching(key),
+        value.map((val) => `./${path.join(baseUrl, takeOutMatching(val))}`),
+      ])
+    }
+  })
+
+  return result
+}
+
 module.exports = {
   // 以当前目录为根目录，不再向上查找 .eslintrc.js
   root: true,
@@ -28,11 +92,11 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
       alias: {
-        map: [['@', './src']],
-        extensions: ['.js', '.jsx'],
+        map: getAliasMap(),
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
--- typescript.js
+++ typescript.js
@@ -1,67 +1,3 @@
-const fs = require('fs')
-const path = require('path')
-const resolve = require('resolve')
-
-const appDirectory = fs.realpathSync(process.cwd())
-const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath)
-
-const filePaths = {
-  appPath: resolveApp('.'),
-  appTsConfig: resolveApp('tsconfig.json'),
-  appJsConfig: resolveApp('jsconfig.json'),
-  appNodeModules: resolveApp('node_modules'),
-}
-
-// 去掉通配符
-const takeOutMatching = (str) => (str || '').replace(/\/\*.*/, '')
-
-function getAliasMap() {
-  // 是否存在js/ts配置文件
-  const hasTsConfig = fs.existsSync(filePaths.appTsConfig)
-  const hasJsConfig = fs.existsSync(filePaths.appJsConfig)
-
-  if (hasTsConfig && hasJsConfig) {
-    throw new Error('tsconfig.json 和 jsconfig.json同时存在，请删除其一')
-  }
-
-  let config
-
-  // tsconfig优先级比较高
-  if (hasTsConfig) {
-    const ts = require(resolve.sync('typescript', {
-      basedir: filePaths.appNodeModules,
-    }))
-    config = ts.readConfigFile(filePaths.appTsConfig, ts.sys.readFile).config
-  } else if (hasJsConfig) {
-    // jsconfig
-    config = require(filePaths.appJsConfig)
-  }
-
-  // 未找到配置文件
-  if (!config) {
-    return []
-  }
-
-  config = config || {}
-  const options = config.compilerOptions || {}
-  const { baseUrl, paths } = options
-  if (!baseUrl || !paths) {
-    return []
-  }
-
-  const result = []
-  Object.entries(paths).forEach(([key, value]) => {
-    if (Array.isArray(value) && value.length > 0) {
-      result.push([
-        takeOutMatching(key),
-        value.map((val) => `./${path.join(baseUrl, takeOutMatching(val))}`),
-      ])
-    }
-  })
-
-  return result
-}
-
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -91,15 +27,6 @@ module.exports = {
     'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      alias: {
-        map: getAliasMap(),
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
     },
   },
 }
